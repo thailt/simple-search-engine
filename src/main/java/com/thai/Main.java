d6e39a3e08ac1cab4a545f08ee7ca350ff72e24f
Le Tri Thai <thai1225@gmail.com>
2018-11-27 16:37:58 +0700

    add spring

--- src/main/java/com/thai/Main.java
+++ src/main/java/com/thai/Main.java
@@ -1,7 +1,18 @@
 package com.thai;
 
-public class Main {
-    public static void main(String[] args) {
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
+public class Main implements CommandLineRunner {
+    public static void main(String[] args) {
+        new SpringApplication(Main.class).run(args);
+    }
+
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("hello");
     }
 }
