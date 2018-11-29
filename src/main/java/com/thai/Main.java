3be022731711427e2c0ea02f86ba2ae1de689113
Le Tri Thai <thai1225@gmail.com>
2018-11-29 11:48:11 +0700

    simple bench

--- src/main/java/com/thai/Main.java
+++ src/main/java/com/thai/Main.java
@@ -1,11 +1,15 @@
 package com.thai;
 
 import com.thai.search.SearchEngine;
+import com.thai.util.Constants;
+import com.thai.util.StringUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.List;
 
 @SpringBootApplication
 public class Main implements CommandLineRunner {
@@ -24,6 +28,46 @@ public class Main implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        System.out.println("hello");
+        String absoluteFilePath = StringUtil
+                .getAbsoluteResourceFilePath(Constants.productNameTextFile);
+        List<String> texts = StringUtil.readLines(absoluteFilePath);
+        indexBench(fullTextSearch, texts);
+        indexBench(keywordSearch, texts);
+
+        String queryTextFileAbsoluteFilePath = StringUtil
+                .getAbsoluteResourceFilePath(Constants.queryTextFile);
+        List<String> queryTexts = StringUtil.readLines(queryTextFileAbsoluteFilePath);
+
+        searchBench(fullTextSearch, queryTexts);
+        searchBench(keywordSearch, queryTexts);
+
+    }
+
+    private void indexBench(SearchEngine searchEngine, List<String> texts) {
+        Long startAt = System.nanoTime();
+        searchEngine.index(texts);
+
+        Long endAt = System.nanoTime();
+        Long duration = endAt - startAt;
+
+        System.out.println(String
+                .format("duration for class %s, functions %s is %f ms ",
+                        searchEngine.getClass().getCanonicalName(), "indexBench", duration / 1000000.0));
+
+    }
+
+    private void searchBench(SearchEngine engine, List<String> texts) {
+        Long startAt = System.nanoTime();
+
+        for (String text : texts) {
+            List<String> results = engine.search(text);
+        }
+
+        Long endAt = System.nanoTime();
+        Long duration = endAt - startAt;
+
+        System.out.println(String
+                .format("duration for class %s, functions %s is %f ms ",
+                        engine.getClass().getCanonicalName(), "searchBench", duration / 1000000.0));
     }
 }
